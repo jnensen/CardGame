345ee8d93b5df075c683237c7730c893c9944c28
Johan Nensen <jnensen@hotmail.com>
2018-11-15 15:22:03 +0100

    4545

---
+++ JavaScriptFeedback.js
@@ -0,0 +1,18 @@
+﻿
+$('form').submit(function (e) {
+    let textInput = $('#textId');
+    let colorInput = $('#colorId');
+    let newRow = $('<tr>');
+    let textCell = $('<td>');
+    let colorCell = $('<td>');
+
+    textCell.text(textInput.val());
+    colorCell.text(colorInput.val());
+    colorCell.css('background-color', colorInput.val());
+
+    newRow.append(textCell);
+    newRow.append(colorCell);
+    $('tbody').append(newRow);
+
+    e.preventDefault();
+});
